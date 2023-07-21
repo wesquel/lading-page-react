74a3850467459369462aff037a48b71169dfc7a6
Weslley Addson <wesquel@hotmail.com>
2023-07-21 17:08:44 -0300

    Adicionando Styled Components

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./templates/App";
+import { GlobalStyles } from "./styles/global-styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
 );
