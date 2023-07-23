d06ac49b6ddec82eea2673e499af4e55a1fa7913
Weslley Addson <wesquel@hotmail.com>
2023-07-23 12:40:44 -0300

    adicionando theme default

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./templates/App";
+import { ThemeProvider } from "styled-components";
+
 import { GlobalStyles } from "./styles/global-styles";
+import { theme } from "./styles/theme";
+import Home from "./templates/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
-    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 );
