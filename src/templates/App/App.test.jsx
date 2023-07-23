33b7b59a0bfaa8e2155b27262f6f8c446f355c33
Weslley Addson <wesquel@hotmail.com>
2023-07-23 13:11:11 -0300

    criando teste para home

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,8 +1,11 @@
-import { render, screen } from "@testing-library/react";
 import Home from ".";
+import { screen } from "@testing-library/react";
+import { renderTheme } from "../../styles/render-theme";
 
 test("renders learn react link", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { debug } = renderTheme(<Home />);
+  debug();
+  const headingContainer = screen.getByRole("heading", { name: "Hello" }).parentElement;
+  //expect(headingContainer).toHaveStyleRule("backgorund", "red");
+  expect(headingContainer).toMatchSnapshot();
 });
