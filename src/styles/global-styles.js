8745d644f2d7560c4f4bf8c66ebe2f358e152943
Weslley Addson <wesquel@hotmail.com>
2023-07-26 21:25:41 -0300

    removendo @link para dentro do html

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -3,10 +3,10 @@ import { theme } from "./theme";
 
 export const GlobalStyles = createGlobalStyle`
 
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
-  * {
-
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 
   html {
@@ -17,7 +17,7 @@ export const GlobalStyles = createGlobalStyle`
     font-family: ${({ theme }) => theme.fonts.family.default};
   }
 
-  h1 {
+  h1,h2,h3,h4,h5,h6 {
     font-size: 4.5rem;
     font-family: ${({ theme }) => theme.fonts.family.secondary};
   }
