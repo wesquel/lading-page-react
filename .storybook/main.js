5049fad1bffa1a4136d34ed672a8cfc46d3a4785
Weslley Addson <wesquel@hotmail.com>
2023-07-26 21:25:58 -0300

    removendo warning do storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
