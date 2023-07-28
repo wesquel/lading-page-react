cb68b655a5935acccf24ef9773af8d4309712711
Weslley Addson <wesquel@hotmail.com>
2023-07-28 02:36:41 -0300

    Adicionando os testes da Heading

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -2,10 +2,8 @@ import Home from ".";
 import { screen } from "@testing-library/react";
 import { renderTheme } from "../../styles/render-theme";
 
-test("renders learn react link", () => {
-  const { debug } = renderTheme(<Home />);
-  debug();
-  const headingContainer = screen.getByRole("heading", { name: "Hello" }).parentElement;
-  //expect(headingContainer).toHaveStyleRule("backgorund", "red");
-  expect(headingContainer).toMatchSnapshot();
+describe("<Home/>", () => {
+  it("should render home", () => {
+    renderTheme(<Home />);
+  });
 });
