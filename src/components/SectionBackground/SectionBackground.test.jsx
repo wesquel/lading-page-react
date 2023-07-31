f0240e5216367393b61387bf0de24dd609adfb3c
Weslley Addson <wesquel@hotmail.com>
2023-07-31 13:46:31 -0300

    adicionando testes

--- src/components/SectionBackground/SectionBackground.test.jsx
+++ src/components/SectionBackground/SectionBackground.test.jsx
@@ -1,10 +1,25 @@
-//import { screen } from "@testing-library/react";
-import { renderTheme } from "../../styles/render-theme";
+import { screen } from "@testing-library/react";
 import { SectionBackground } from ".";
+import { renderTheme } from "../../styles/render-theme";
 
 describe("<SectionBackground />", () => {
-  it("should render", () => {
-    renderTheme(<SectionBackground>Children</SectionBackground>);
-    //expect(screen.getByRole("heading")).toBeInTheDocument();
+  it("should render with background dark", () => {
+    const { container } = renderTheme(
+      <SectionBackground background={true}>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render with background light", () => {
+    const { container } = renderTheme(
+      <SectionBackground>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
--- src/components/SectionContainer/SectionContainer.test.jsx
+++ src/components/SectionContainer/SectionContainer.test.jsx
@@ -1,10 +1,15 @@
-//import { screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { renderTheme } from "../../styles/render-theme";
 import { SectionContainer } from ".";
 
 describe("<SectionContainer />", () => {
-  it("should render", () => {
-    renderTheme(<SectionContainer>Children</SectionContainer>);
-    //expect(screen.getByRole("heading")).toBeInTheDocument();
+  it("should render content", () => {
+    const { container } = renderTheme(
+      <SectionContainer>
+        <h1>Children</h1>
+      </SectionContainer>,
+    );
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
