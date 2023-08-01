fd5adc8113732ba6fd336b8b574d12da0da3642a
Weslley Addson <wesquel@hotmail.com>
2023-08-01 20:35:57 -0300

    criação do componente

---
+++ src/components/Menu/stories.jsx
@@ -0,0 +1,22 @@
+import { Menu } from ".";
+import linksMock from "../NavLinks/mock";
+export default {
+  title: "Menu",
+  component: Menu,
+  args: {
+    links: linksMock,
+    logoData: {
+      text: "Logo",
+      link: "#target",
+      srcImg: "",
+    },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Menu {...args} />
+    </div>
+  );
+};
