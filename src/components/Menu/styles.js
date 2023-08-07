8f2ce734df20803dd05e927145136e87f1ec0112
Weslley Addson <wesquel@hotmail.com>
2023-08-07 10:21:19 -0300

    finalização do componente

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -2,8 +2,13 @@ import styled, { css } from "styled-components";
 import { Container as SectionContainer } from "../SectionContainer/styles";
 import { Title as Heading } from "../SectionContainer/styles";
 
+const menuVisible = () => css`
+  visibility: visible;
+  opacity: 1;
+`;
+
 export const Container = styled.div`
-  ${({ theme }) => css`
+  ${({ theme, visible }) => css`
     ${theme}
     position: fixed;
     z-index: 5;
@@ -13,6 +18,7 @@ export const Container = styled.div`
     bottom: 0;
     border-bottom: ${theme.colors.mediumGray};
     background: ${theme.colors.white};
+    transition: all 300ms ease-in-out;
 
     > ${SectionContainer} {
       padding-top: 0;
@@ -25,6 +31,9 @@ export const Container = styled.div`
 
     @media ${theme.media.lteMedium} {
       height: 100vh;
+      visibility: hidden;
+      opacity: 0;
+      ${visible && menuVisible(theme)}
 
       > ${SectionContainer} {
         display: grid;
@@ -45,8 +54,6 @@ export const Container = styled.div`
 
 export const MenuContainer = styled.div`
   ${({ theme }) => css`
-    ${theme}
-
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -60,7 +67,26 @@ export const MenuContainer = styled.div`
 `;
 
 export const Button = styled.button`
-  ${({ theme }) => css`
-    ${theme}
+  ${({ theme, visible }) => css`
+    z-index: 6;
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    width: 4rem;
+    height: 4rem;
+    background: ${theme.colors.primaryColor};
+    color: ${theme.colors.white};
+    border: none;
+    display: none;
+    pointer-events: ${visible ? "none" : "all"};
+    @media ${theme.media.lteMedium} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    > svg {
+      width: 2.5rem;
+      height: 2.5rem;
+    }
   `}
 `;
